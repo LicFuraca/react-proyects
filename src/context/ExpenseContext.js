70067aadf701507ed3b760670cee86a8b79de063
LicFuraca <martinexequield@gmail.com>
2023-04-26 21:38:34 -0300

    Borré un import a Link que no se utilizaba, y modifiqué la utilización de Local Storage

--- src/context/ExpenseContext.js
+++ src/context/ExpenseContext.js
@@ -3,32 +3,33 @@ import { createContext, useState, useEffect } from "react";
 const ExpenseContext = createContext();
 
 export const ExpenseProvider = ({ children }) => {
-  let localData = localStorage.getItem("transactions");
-  const [transactions, setTransactions] = useState(JSON.parse(localData));
+	const [transactions, setTransactions] = useState([]);
 
-  const addTransaction = (title, amount) => {
-    setTransactions([...transactions, { title, amount, id: Math.random() }]);
-  };
+	const addTransaction = (title, amount) => {
+		setTransactions([...transactions, { title, amount, id: Math.random() }]);
+	};
 
-  const deleteTransaction = id => {
-    setTransactions(transactions.filter(transaction => transaction.id !== id));
-  };
+	const deleteTransaction = (id) => {
+		setTransactions(
+			transactions.filter((transaction) => transaction.id !== id)
+		);
+	};
 
-  useEffect(() => {
-    const getTransactions = () => {
-      localStorage.setItem("transactions", JSON.stringify(transactions));
-    };
+	useEffect(() => {
+		const getTransactions = () => {
+			localStorage.setItem("transactions", JSON.stringify(transactions));
+		};
 
-    getTransactions();
-  }, [transactions]);
+		getTransactions();
+	}, [transactions]);
 
-  return (
-    <ExpenseContext.Provider
-      value={{ transactions, deleteTransaction, addTransaction }}
-    >
-      {children}
-    </ExpenseContext.Provider>
-  );
+	return (
+		<ExpenseContext.Provider
+			value={{ transactions, deleteTransaction, addTransaction }}
+		>
+			{children}
+		</ExpenseContext.Provider>
+	);
 };
 
 export default ExpenseContext;
